2c789f4fee92605ce25b0cc4250639ae483e2c4d
faisbot021 <105857615+faisbot021@users.noreply.github.com>
2022-05-20 09:38:09 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -2,18 +2,10 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(image + 'donasi')).buffer(), `
 ╭──『 _*Donasi*_ 』──⬣
 │⬡ *_Dana/Gopay_* :
-│⬡ *088233832771*
-│
+│⬡ *081959293465*
 │⬡ *_Pulsa_* :
-│⬡ *089612698583*
-│⬡ *088233832771*
+│⬡ *081959293465*
 ╰─────────⬣
-╭─「 *NOTE* 」
-│ > Owner? wa.me/6288233832771
-│ _Hasil donasi akan digunakan buat sewa_
-│ _atau beli *RDP/VPS* agar bot bisa jalan_
-│ _24jam tanpa kendala_
-╰──────────⬣
 `.trim(), footer, 'Owner', '.owner')
 handler.help = ['donasi']
 handler.tags = ['info']
